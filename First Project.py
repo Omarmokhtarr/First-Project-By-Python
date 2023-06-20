3af600433bb2be320d710899c7064ef1196c4e43
Omarmokhtarr <98532974+Omarmokhtarr@users.noreply.github.com>
2023-06-20 22:20:04 +0300

    Add files via upload

---
+++ First Project.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # First Project By Python
+
+# Welcome in my first project by python
+# 
+# 
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Data Science Languages:
+R
+Python
+SQL
+Java
+Julia
+Scala
+JavaScript
+Swift
+# Data Science libraries:   
+    NumPy
+    Matbotlib
+    Seaborn
+    Scikit-learn
+    TensorFlow
+    Keras
+    PyTorch
+    Pandas
+# Data Science tools:
+# 
+Apache Spark 
+SAS 
+BigML 
+D3.js 
+MATLAB 
+ggplot2 
+Tableau 
+TensorFlow
+Weka 
+IBM Watson
+# Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+code = compile("5 + 4", "<string>", "eval")
+eval(code)
+
+
+# In[3]:
+
+
+import math
+code2 = compile("4 / 3 * math.pi * math.pow(25, 3)", "<string>", "eval")
+eval(code2)
+
+
+# 
+# 
+# This a simple arithmetic expression to mutiply then add integers
+# 
+
+# In[4]:
+
+
+(3*4)+5
+
+
+# 
+# 
+# This will convert 200 minutes to hours by diving by 60
+# 
+
+# In[6]:
+
+
+days = 0
+hours = 0
+mins = 0
+time = 200
+leftover_minutes = time % 1440
+hours = leftover_minutes / 60
+print(str(days) + " days, " + str(hours) + " hours, " + str(mins) +  " mins. ")
+
+
+# popular languages for Data Science
+1.R
+2.Python
+3.SQL
+4.Java
+# Author:
+
+# Omar Mokhtar
